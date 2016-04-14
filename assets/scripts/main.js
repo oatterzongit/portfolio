8443807e283f092be6990062b43950d63e4fa33d
Oat Asdon <o.asdon@gmail.com>
2016-04-14 10:39:54 -0700

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,12 @@
+// Main
+
+$(document).ready(function(){
+  console.log('DOM Ready');
+
+  $('.parallax').parallax();
+
+  $(".button-collapse").sideNav();
+
+  $('.slider').slider({full_width: true});
+
+});
